840d7f59adc46ece3223fe749a357008f1b21487
Az-21 <39803478+Az-21@users.noreply.github.com>
2023-11-15 22:54:19 +0530

    Remove about page

--- healthcare/app/lib/routes.dart
+++ healthcare/app/lib/routes.dart
@@ -1,9 +1,6 @@
 import 'package:go_router/go_router.dart';
-import 'package:mhrc/about/page.dart';
 import 'package:mhrc/home/page.dart';
 
 final router = GoRouter(routes: [
-  GoRoute(path: "/", builder: (context, state) => const HomePage(), routes: [
-    GoRoute(path: "about", builder: (context, state) => const AboutPage()),
-  ]),
+  GoRoute(path: "/", builder: (context, state) => const HomePage()),
 ]);
